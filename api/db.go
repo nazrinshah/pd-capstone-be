3dbaa1ab92e8b298f2dac358c61e341d59c92925
nazrin-shah <nazrin.shah@foodpanda.com>
2023-03-30 06:55:51 +0800

    add db stuff

---
+++ api/db.go
@@ -0,0 +1,14 @@
+package api
+
+type DB struct {
+}
+
+const (
+	DB_HOST     = "http://localhost"
+	DB_PORT     = "3306"
+	DB_SCHEMA   = "capstone"
+	DB_USERNAME = "root"
+	DB_PASSWORD = "P@ss1234"
+	TABLE_DISH  = "dish_tab"
+	TABLE_ORDER = "order_tab"
+)
